27da95c74020ca4fe7125581e95007170255930c
MartenYeager <marten.ter.meer@gmail.com>
2024-08-12 12:04:23 +0200

    innit code
    A starting project with a small start to Tkinter

---
+++ pythonTkinter/main.py
@@ -0,0 +1,17 @@
+import keyboard
+import tkinter as tk
+
+def innitWindow():
+    root = tk.Tk()
+    root.geometry('300x300')
+
+    root.mainloop()
+
+def main():
+    innitWindow()
+
+
+main()
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
