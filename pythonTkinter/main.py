fa4fc20884f3083d6eb292d5b55fc2645980aff5
MartenYeager <marten.ter.meer@gmail.com>
2024-08-12 15:40:31 +0200

    Innit window and grid
    -Experimented with the workings of the grid in main(), grid will be moved to more appropriate location.
    -Added class 'gridUnit' for the contents of the grid

--- pythonTkinter/main.py
+++ pythonTkinter/main.py
@@ -1,17 +1,53 @@
 import keyboard
+import numpy
 import tkinter as tk
+
+class gridUnit:
+    #a grid unit is 50 X 50 (500 / 10)
+    content = None
+    gridX = None
+    gridY = None
+    center = None
+    
+    def __init__(self, content, size, gridX, gridY):
+        self.content = content  # EMPTY, PALLET, WALL
+        self.gridX = int(gridX)
+        self.gridY = int(gridY)
+        self.center = [gridX * size, gridY * size]  #This puts the center in the bottom left corner
+
+    def setContent(self, content):  #Used to update grids with eaten pallets
+        self.content = content
 
 def innitWindow():
     root = tk.Tk()
-    root.geometry('300x300')
+    root.geometry('500x500')
+
+    canvas = tk.Canvas(root, width=500, height=500, background='black')
+    #drawing code
+    canvas.pack()
 
     root.mainloop()
 
 def main():
-    innitWindow()
+    #innitWindow()
+
+    gridSize = 10
+    grid = numpy.ndarray(shape=(gridSize, gridSize), dtype=gridUnit)
+
+    for gridY in range(10):
+        for gridX in range(10):
+            #print(gridX, ' : ', gridY)
+
+            if (gridX == 0 or gridX == 9 or gridY == 0 or gridY == 9):
+                #print('WALL')
+                content = 'WALL'
+            else:
+                content = 'EMTPY'
+            grid[gridX][gridY] = gridUnit(content, 50, gridX, gridY)
+
+    for gridY in range(10):
+        for gridX in range(10):
+            print(grid[gridX][gridY].content)
 
 
 main()
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
