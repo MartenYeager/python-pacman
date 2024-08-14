54c37e0ac7f5e33f9505694c42e5eab172400ee8
MartenYeager <marten.ter.meer@gmail.com>
2024-08-14 16:37:23 +0200

    Update main.py
    -Removed unused variables from gridUnit class
    -Change location calculations
    -Initial placement of spawns

--- pythonTkinter/main.py
+++ pythonTkinter/main.py
@@ -6,15 +6,15 @@ class gridUnit:
     #a grid unit is 50 X 50 (500 / 10)
     content = None
     canvasID = None
-    gridX = None
-    gridY = None
+    #gridX = None
+    #gridY = None
     center = None
     
     def __init__(self, content, size, gridX, gridY):
         self.content = content  # EMPTY, PALLET, WALL
-        self.gridX = int(gridX)
-        self.gridY = int(gridY)
-        self.center = [gridX * size, gridY * size]  #This puts the center in the bottom left corner
+        #self.gridX = int(gridX)
+        #self.gridY = int(gridY)
+        self.center = (gridX * size + size / 2, gridY * size + size / 2)
 
     def setContent(self, content):  #Used to update grids with eaten pallets
         self.content = content
@@ -28,7 +28,7 @@ def meetsWallCondition(x, y):
         (x == 2 and (y >= 2 and y <= 4)) or
         (x == 8 and (y >= 6 and y <= 8)) or
         (x == 8 and (y >= 2 and y <= 4)) or
-        #((x >= 4 and y <= 6) and y == 8) or
+
         (y == 8 and (x >= 4 and x <= 6)) or
 
         #Home base
@@ -37,18 +37,26 @@ def meetsWallCondition(x, y):
         (x == 5 and y == 6)
     )
     return wallCondition
+
+
+def meetsEmptyCondition(x, y):
+    emptyCondition = (
+        (x == 5 and y == 5) or
+        (x == 5 and y == 4) or
+        (x == 1 and y == 5)
+    )
+    return emptyCondition
 def innitGrid(gridSize, unitSize):
     grid = numpy.ndarray(shape=(gridSize[0], gridSize[1]), dtype=gridUnit)
 
-    #Creates the WALL's of the maze
     for gridX in range(len(grid)):
         for gridY in range(len(grid)):
             # print(gridX, ' : ', gridY)
 
             if (gridX == 0 or gridX == len(grid) - 1 or gridY == 0 or gridY == len(grid) - 1) or meetsWallCondition(gridX, gridY):
                 content = 'WALL'
-            #elif gridX % 2 != 0 or gridY % 2 != 0:
-                #content = 'PALLET'
+            elif meetsEmptyCondition(gridX, gridY):
+                content = 'EMPTY'
             else:
                 content = 'PALLET'
 
@@ -57,35 +65,45 @@ def innitGrid(gridSize, unitSize):
     return grid
 
 def drawGrid(grid, canvas):
-    varB = 15
+    sizeModifier = 15
     unitSize = 50
 
     for gridX in range(len(grid)):
         for gridY in range(len(grid)):
-            if grid[gridX][gridY].content == 'WALL':
-                id = canvas.create_rectangle(gridX * unitSize, gridY * unitSize, (gridX + 1) * unitSize, (gridY + 1) * unitSize, fill='blue')
-            elif grid[gridX][gridY].content == 'PALLET':
-                id = canvas.create_oval((gridX * unitSize) + varB, (gridY * unitSize) + varB, ((gridX + 1) * unitSize) - varB, ((gridY + 1) * unitSize) - varB, fill='beige')
-            grid[gridX][gridY].setCanvasID(id)
+            unit = grid[gridX][gridY]
+            if unit.content == 'WALL':
+                #id = canvas.create_rectangle(gridX * unitSize, gridY * unitSize, (gridX + 1) * unitSize, (gridY + 1) * unitSize, fill='blue')
+                id = canvas.create_rectangle(unit.center[0] - unitSize / 2, unit.center[1] - unitSize / 2, unit.center[0] + unitSize / 2, unit.center[1] + unitSize / 2, fill='blue')
+            elif unit.content == 'PALLET':
+                id = canvas.create_oval(unit.center[0] - unitSize / 2 + sizeModifier, unit.center[1] - unitSize / 2 + sizeModifier, unit.center[0] + unitSize / 2 - sizeModifier, unit.center[1] + unitSize / 2 - sizeModifier, fill='beige')
+            unit.setCanvasID(id)
 
 def innitWindow():
     root = tk.Tk()
-    root.geometry('550x550')    # To bad this is hard coded
+    screenWidth = 550
+    screenHeight = 550
+    screenDimensions = str(screenWidth) + 'x' + str(screenHeight)
+    root.geometry(screenDimensions)
 
-    varA = 10
-
+    sizeModifier = 10
 
     gridSize = (11, 11)
     unitSize = 50
     grid = innitGrid(gridSize, unitSize)
 
-    # Creates a new canvas for the player
-    #player = movingCanvas(root)  # canvas.create_oval(50 + varA, 250 + varA, 100 - varA, 300 - varA, fill='red')
-    #player.pack(fill='both')
-
     canvasBackground = tk.Canvas(root, width=550, height=550, background='white')
     drawGrid(grid, canvasBackground)
-    temp = canvasBackground.create_oval(250 + varA, 250 + varA, 300 - varA, 300 - varA, fill='red')  #Center
+
+    spawn = grid[1][5].center
+    spawn1 = grid[9][1].center
+    spawn2 = grid[9][9].center
+    spawn3 = grid[7][5].center
+    offset = unitSize / 2
+
+    temp = canvasBackground.create_oval(spawn[0] - offset + sizeModifier, spawn[1] - offset + sizeModifier, spawn[0] + offset - sizeModifier, spawn[1] + offset - sizeModifier, fill='gold')
+    canvasBackground.create_oval(spawn1[0] - offset + sizeModifier, spawn1[1] - offset + sizeModifier, spawn1[0] + offset - sizeModifier, spawn1[1] + offset - sizeModifier, fill='red')
+    canvasBackground.create_oval(spawn2[0] - offset + sizeModifier, spawn2[1] - offset + sizeModifier, spawn2[0] + offset - sizeModifier, spawn2[1] + offset - sizeModifier, fill='red')
+    canvasBackground.create_oval(spawn3[0] - offset + sizeModifier, spawn3[1] - offset + sizeModifier, spawn3[0] + offset - sizeModifier, spawn3[1] + offset - sizeModifier, fill='red')
     canvasBackground.pack()
 
     root.bind("<KeyPress-Left>", lambda _: canvasBackground.move(temp, -5, 0))
